4151a65043b693e5c7c339033e7866163fa44057
Alberto González Balderas <albertoglezb@hotmail.com>
2019-10-22 10:00:52 -0500

    Se terminó la clase Trabajando con React. Se creó el componente loader y se utilizó en la app para renderisar algnos loaders aprendidos en el curso.

--- react/src/js/components/app.js
+++ react/src/js/components/app.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './loader.js'
 
-function App(){
-    return(
+console.log(data)
+
+function App() {
+
+    const [loaderList, setLoaderList] = useState([])
+
+    function hadleClick() {
+        setLoaderList(data.loaders)
+    }
+
+    return (
         <div>
             Que buena aplicación hecha con React.js
+            <ul>
+                {
+                    loaderList.map(item =>
+                        <Loader
+                            {...item}
+                            key={item.id} />
+                    )
+                }
+            </ul>
+            <button onClick={hadleClick}>Cargar loaders</button>
         </div>
     )
 }
